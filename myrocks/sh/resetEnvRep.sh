06cad543bfd3a3b1d5cdcb5b55f2eeedd5658f7a
dleeyh <daniel.lee@mariadb.com>
2016-03-09 15:13:07 -0600

    Improved if logic in the reset environment scripts

--- myrocks/sh/resetEnvRep.sh
+++ myrocks/sh/resetEnvRep.sh
@@ -1,37 +1,36 @@
 #! /bin/bash
 #
 # resetEnvRep.sh applies only when MYRRUNMODE is set to 1
-   if [ "$MYRRUNMODE" != "1" ]; then
-      exit
-   fi   
+   if [ "$MYRRUNMODE" = "1" ]; then
 #
 # setup directories
-   buildName=$1
-   if [ "$buildName" = "" ]; then
-      echo Missing build name
-      echo Usage: $0 reptest
-      exit 
-   fi
+      buildName=$1
+      if [ "$buildName" = "" ]; then
+         echo Missing build name
+         echo Usage: $0 reptest
+         exit 
+      fi
 #
-   testDir=$MYRRELHOME/$buildName
-   echo Module: $0 TestDir=$testDir
+      testDir=$MYRRELHOME/$buildName
+      echo Module: $0 TestDir=$testDir
 #
 # stop mysqld servers if exist
-   killall mysqld
-   sleep 3
+      killall mysqld
+      sleep 3
 #
 # Get a fresh copy of the databases
-   echo 1. Copying a fresh copy of databases
-   $MYRHOME/myrocks/sh/copyDBRep.sh $buildName
+      echo 1. Copying a fresh copy of databases
+      $MYRHOME/myrocks/sh/copyDBRep.sh $buildName
 #
 # Start replication stack, master and slave
-   echo 2. Starting replication stack
-   $MYRHOME/myrocks/sh/startEnvRep.sh $buildName
+      echo 2. Starting replication stack
+      $MYRHOME/myrocks/sh/startEnvRep.sh $buildName
 #
 # Sync replication between master and slave
-  echo 3. Syncing replication master and slave
-  sleep 3
-   $MYRHOME/myrocks/sh/syncEnvRep.sh $buildName
+      echo 3. Syncing replication master and slave
+      sleep 3
+      $MYRHOME/myrocks/sh/syncEnvRep.sh $buildName
 #
-   ps -ef |grep mysqld
+      ps -ef |grep mysqld
 #
+   fi
--- myrocks/sh/resetEnvSingle.sh
+++ myrocks/sh/resetEnvSingle.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 #
 # resetEnvSingle.sh applies only when MYRRUNMODE is set to 1
-   if [ "$MYRRUNMODE" != "1" ]; then
-      exit
-   fi   
+   if [ "$MYRRUNMODE" = "1" ]; then
 #
 # setup directories
-   buildName=$1
-   if [ "$buildName" = "" ]; then
-      echo Missing build name
-      echo Usage: $0 mytest
-      exit 
-   fi
+      buildName=$1
+      if [ "$buildName" = "" ]; then
+         echo Missing build name
+         echo Usage: $0 mytest
+         exit 
+      fi
 #
-   testDir=$MYRRELHOME/$buildName
-   echo Module: $0 TestDir=$testDir
+      testDir=$MYRRELHOME/$buildName
+      echo Module: $0 TestDir=$testDir
 #
 # stop mysqld servers if exist
-   killall mysqld
-   sleep 3
+      killall mysqld
+      sleep 3
 #
 # Get a fresh copy of the databases
-   echo 1. Copying a fresh copy of databases
-   $MYRHOME/myrocks/sh/copyDBSingle.sh $buildName
+      echo 1. Copying a fresh copy of databases
+      $MYRHOME/myrocks/sh/copyDBSingle.sh $buildName
 #
 # Start single stack, master and slave
-   echo 2. Starting single stack
-   $MYRHOME/myrocks/sh/startEnvSingle.sh $buildName
-   sleep 3
+      echo 2. Starting single stack
+      $MYRHOME/myrocks/sh/startEnvSingle.sh $buildName
+      sleep 3
 #
-   ps -ef |grep mysqld
+      ps -ef |grep mysqld
+#
+   fi
 #
